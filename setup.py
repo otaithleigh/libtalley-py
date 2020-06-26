33f6632f5817a5776ea2b1d2873e95ba13fda9c8
Peter Talley <peterctalley@gmail.com>
2020-06-26 01:38:42 -0400

    Switch to declarative config

--- setup.py
+++ setup.py
@@ -1,28 +1,2 @@
 from setuptools import setup
-setup(
-    name="libtalley",
-    version="0.7.2",
-    packages=[
-        'libtalley',
-        'libtalley.asce7_16',
-    ],
-    package_data={
-        'libtalley': [
-            'aisc-shapes-database-v15-0-SI.csv.bz2',
-            'aisc-shapes-database-v15-0-US.csv.bz2',
-            'steel-materials.csv',
-        ]
-    },
-    install_requires=[
-        'numpy >= 1.15.0',
-        'pandas',
-        'scipy >= 1.0.0',
-        'tabulate',
-        'unyt',
-    ],
-    python_requires=">=3.7.0",
-    author="Peter Talley",
-    author_email="peterctalley@gmail.com",
-    description="A collection of helpful functions and doodads.",
-    license="MIT",
-)
+setup()
