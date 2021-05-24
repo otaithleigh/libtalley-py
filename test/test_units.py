815f590c16b97737970789f358b8e04308833bca
Peter Talley <peterctalley@gmail.com>
2021-05-24 12:09:22 -0400

    Add tests for new features

--- test/test_units.py
+++ test/test_units.py
@@ -1,7 +1,13 @@
+import numpy as np
 import pytest
 import unyt
 from unyt.exceptions import UnitConversionError
 from unyt.testing import assert_allclose_units
+
+try:
+    import xarray as xr
+except ImportError:
+    xr = None
 
 from libtalley import units
 from libtalley.units import UnitInputParser, get_unit_system, process_unit_input
@@ -49,7 +55,54 @@ def test_parse_units_bad_tuple():
 def test_parse_units_already_unyt():
     in_ = unyt.unyt_array([1, 2, 3, 4], 'ft')
     out_ = process_unit_input(in_)
+    assert_allclose_units(in_, out_)
+
+
+def test_parse_units_already_unyt_no_copy():
+    in_ = unyt.unyt_array([1, 2, 3, 4], 'ft')
+    out_ = process_unit_input(in_, copy=False)
     assert in_ is out_
+
+
+def test_parse_units_array():
+    in_ = np.array([1, 2, 3, 4])
+    actual = process_unit_input(in_, 'ft')
+    desired = unyt.unyt_array([1, 2, 3, 4], 'ft')
+    assert actual.base is not in_
+    assert_allclose_units(actual, desired)
+
+
+def test_parse_units_no_copy_array():
+    in_ = np.array([1, 2, 3, 4])
+    actual = process_unit_input(in_, 'ft', copy=False)
+    desired = unyt.unyt_array([1, 2, 3, 4], 'ft')
+    assert actual.base is in_
+    assert_allclose_units(actual, desired)
+
+
+if xr is not None:
+    def test_parse_units_xarray():
+        in_ = xr.DataArray([1, 2, 3, 4], attrs={'units': 'mm'})
+        actual = process_unit_input(in_)
+        desired = unyt.unyt_array([1, 2, 3, 4], 'mm')
+        assert_allclose_units(actual, desired)
+
+    def test_parse_units_xarray_no_copy():
+        in_ = xr.DataArray([4, 3, 2, 1], attrs={'units': 'm'})
+        actual = process_unit_input(in_, copy=False)
+        desired = unyt.unyt_array([4, 3, 2, 1], 'm')
+        assert actual.base is in_.values
+        assert_allclose_units(actual, desired)
+
+    def test_parse_units_xarray_no_units():
+        in_ = xr.DataArray([4, 3, 2, 1])
+        with pytest.raises(ValueError):
+            process_unit_input(in_)
+
+    def test_parse_units_xarray_no_accept_bad_casing():
+        in_ = xr.DataArray([4, 3, 2, 1], attrs={'Units': 'm'})
+        with pytest.raises(ValueError):
+            process_unit_input(in_)
 
 
 def test_parse_units_override_default():
